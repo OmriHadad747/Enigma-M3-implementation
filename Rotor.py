0d0895117c00b4d68bd4c5fecd7c3e2cbf5401e1
OmriHadad747 <omrihadad747@gmail.com>
2018-11-06 01:13:06 +0200

    6/11/2018 - 01:13

--- Rotor.py
+++ Rotor.py
@@ -1,18 +1,16 @@
 from Translator import Translator
 
-
-
+# this class represent a single rotor
 class Rotor(Translator):
-
-    SIZE = 25
-    permutation = []
-    reversePermutation = [SIZE]
+    SIZE = 26
+    direction = "FORWARD"
 
     def __init__(self, name, permutation, turnOver):
         self.name = name
         self.permutation = permutation
-        self.reverseTranslation()
-        self.turnOver = turnOver
+        self.reversePermutation = [0] * self.SIZE
+        self.reverseTranslation()  # create reverse translation from forward permutation
+        self.turnOver = Translator.letterToIndex(turnOver)
         self.setting = 0
         self.offset = 0
 
@@ -22,7 +20,7 @@ class Rotor(Translator):
     def setOffset(self, offset):
         self.offset = offset
 
-    #create reverse permutation from the forward permutation
+    # create reverse permutation from the forward permutation
     def reverseTranslation(self):
         abc = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
         index = 0
@@ -30,7 +28,30 @@ class Rotor(Translator):
             self.reversePermutation[Translator.letterToIndex(letter)] = abc[index]
             index += 1
 
-        print self.reversePermutation
+    # encrypt a given letter
+    def translation(self, letter):
+        shift = self.circularShifts(letter, self.offset - self.setting)
+        if self.direction == 'REVERSE':
+            convert = self.reversePermutation[Translator.letterToIndex(shift)]
+        else:
+            convert = self.permutation[Translator.letterToIndex(shift)]
 
-    def forwardTranslation(self):
-        pass
+        shift = self.circularShifts(convert, (self.setting - 1) - (self.offset - 1))
+        return shift
+
+    def step(self):
+        if self.offset == 25:
+            self.offset = 0
+        self.offset += 1
+
+    def isTurnover(self):
+        if self.offset == self.turnOver:
+            return True
+
+        return False
+
+    def changeDirection(self):
+        if self.direction == 'FORWARD':
+            self.direction = 'REVERSE'
+        else:
+            self.direction = 'FORWARD'
