b50102e1c6aef752d8f04b2f505ae2c0e9442a7d
OmriHadad747 <omrihadad747@gmail.com>
2018-11-06 01:12:05 +0200

    6/11/2018 - 01:11

--- Driver.py
+++ Driver.py
@@ -1,6 +1,23 @@
 from Enigma import Enigma
 
-enigma = Enigma(0, 0, 0, 1, 0, 0, 2, 0, 0, [('A', 'C'), ('B', 'D')])
+def encryptLetter():
+    while True:
+        letter = raw_input("Enter Letter: ")
+        encryptedLetter = enigma.encryptLetter(letter)
+        print "encrypted letter: "
+        print encryptedLetter
 
-#letter = input("Enter Letter: ")
-#enigma.encryptLetter(letter)
+def encryptMessage():
+    message = raw_input("Enter Message: ")
+    for letter in message:
+        print encryptLetter(letter)
+
+enigma = Enigma(0, 0, 0, 1, 0, 0, 2, 0, 0, [('A', 'C')])
+input = raw_input("Encrypt A Full Message Enter-0 \ Encrypt A Single Letter-1: ")
+if input == 0:
+    encryptMessage()
+elif input == 1:
+    encryptLetter()
+
+
+
